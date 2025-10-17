d50a282a07e0d2562c308f100d8fb740ea4952c0
Nishtha Pardeshi <nishthapardesi2005@gmail.com>
2025-10-17 14:54:52 +0530

    old code

--- sl-i (ai)/ai-1.py
+++ sl-i (ai)/ai-1.py
@@ -1,26 +1,63 @@
-from collections import deque
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.children = []
 
-class Node: 
-    def __init__(self, val): 
-        self.val = val 
-        self.children = [] 
+def BFS(root):
+    visited = set()         
+    queue = [root]          
 
-    def BFS(self): 
-        visited = set() 
-        queue = deque([self])  
-        while queue: 
-            node = queue.popleft()  
-            if node not in visited: 
-                print(node.val, end=" ") 
-                visited.add(node) 
-                for child in node.children: 
-                    queue.append(child) 
+    while queue:
+        node = queue.pop(0) 
+        if node not in visited:
+            print(node.val, end=" ")
+            visited.add(node)
 
-    def DFS_Rec(self, visited=None): 
-        if visited is None: 
-            visited = set()
-        if self not in visited:
-            print(self.val, end=" ")  
-            visited.add(self)  
-            for child in self.children: 
-                child.DFS_Rec(visited)
+            for child in node.children:
+                queue.append(child)
+
+def DFS_Rec(node, visited=None):
+    if visited is None:
+        visited = set()
+
+    if node is None or node in visited:
+        return
+
+    print(node.val, end=" ")
+    visited.add(node)
+
+    for child in node.children:
+        DFS_Rec(child, visited)
+
+def DFS(root):
+    visited = set()
+    stack = [root]          
+
+    while stack:
+        current = stack.pop()  
+        if current not in visited:
+            print(current.val, end=' ')
+            visited.add(current)
+
+            for child in reversed(current.children):
+                stack.append(child)
+
+A = Node('A')
+B = Node('B')
+C = Node('C')
+D = Node('D')
+E = Node('E')
+
+A.children = [B, C, D]
+B.children = [A, E]
+C.children = [A, E]
+D.children = [A, E]
+E.children = [B, C, D]
+
+BFS(A)
+
+print("\n\nDFS Iterative:")
+DFS(A)
+
+print("\n\nDFS Recursive:")
+DFS_Rec(A)

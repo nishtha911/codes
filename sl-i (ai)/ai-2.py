00a2ac978d4dd9bbcae12c4c431728079b71285c
Nishtha Pardeshi <nishthapardesi2005@gmail.com>
2025-10-24 19:52:48 +0530

    updated variable names

--- sl-i (ai)/ai-2.py
+++ sl-i (ai)/ai-2.py
@@ -1,17 +1,22 @@
-def astar(graph, heuristic, start, goal):
-    open_list = [start]
-    closed_list = []
+graph = {
+    'A': {'B': 1, 'C': 3},
+    'B': {'A': 1, 'C': 1, 'D': 6},
+    'C': {'A': 3, 'B': 1, 'D': 2, 'E': 3},
+    'D': {'B': 6, 'C': 2, 'E': 1},
+    'E': {'C': 3, 'D': 1}
+}
+
+heuristic = {'A': 1, 'B': 1, 'C': 0, 'D': 1, 'E': 1}
+
+def astar(start, goal):
+    open_list = set([start])
+    visited = set()
+
     g_cost = {start: 0}
     parent = {start: None}
 
     while open_list:
-        current = open_list[0]
-        min_f = g_cost[current] + heuristic[current]
-        for node in open_list:
-            f = g_cost[node] + heuristic[node]
-            if f < min_f:
-                min_f = f
-                current = node
+        current = min(open_list, key=lambda x: g_cost[x] + heuristic[x])
 
         if current == goal:
             path = []
@@ -21,32 +26,19 @@ def astar(graph, heuristic, start, goal):
             return path[::-1]
 
         open_list.remove(current)
-        closed_list.append(current)
+        visited.add(current)
 
-        for neighbor, cost in graph[current]:
-            if neighbor in closed_list:
-                continue
-            new_g = g_cost[current] + cost
-
-            if neighbor not in open_list:
-                open_list.append(neighbor)
-            elif new_g >= g_cost.get(neighbor, 9999):
+        for node in graph[current]:
+            if node in visited:
                 continue
 
-            g_cost[neighbor] = new_g
-            parent[neighbor] = current
+            cost = g_cost[current] + graph[current][node]
+
+            if node not in open_list or cost < g_cost[node]:
+                g_cost[node] = cost
+                parent[node] = current
+                open_list.add(node)
 
     return None
 
-
-graph = {
-    'A': [('B', 1), ('C', 3)],
-    'B': [('A', 1), ('C', 1), ('D', 6)],
-    'C': [('A', 3), ('B', 1), ('D', 2), ('E', 3)],
-    'D': [('B', 6), ('C', 2), ('E', 1)],
-    'E': [('C', 3), ('D', 1)]
-}
-
-heuristic = {'A': 7, 'B': 6, 'C': 2, 'D': 1, 'E': 0}
-
-print("Shortest Path:", astar(graph, heuristic, 'A', 'E'))
+print("Shortest Path:", astar('A', 'C'))
